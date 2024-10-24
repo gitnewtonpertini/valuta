39359bbe186c0bc9a2d237c61f7851e73daefe1c
glamon <gianfranco.lamon@gmail.com>
2024-10-24 21:18:00 +0200

    Lezione di lunedì 21/10/2024: passaggio di riferimenti, copie e costruttori di copia (vedi Cambio.java)

--- Prova.java
+++ Prova.java
@@ -1,27 +1,72 @@
 import java.util.Scanner;
 
-public class Prova{
-    public static void main(String args[]){
+/**
+ * Classe di test per l'applicazione Valuta
+ * 
+ * @author 4A ITI
+ * @since ottobre 2024
+ */
+public class Prova {
+    public static void main(String args[]) {
         float importo;
 
-        // Test classe Valuta
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Inserisci il valore da convertire:");
-        importo=sc.nextFloat();
-        Valuta n1=new Valuta("EUR", 1.023f);
-        float x= n1.converti(importo);
-        System.out.println("Il valore in dollari "+x);
+        // Test conversione
+        System.out.println("\nTEST VALUTA/CONVERSIONE");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Inserisci il valore da convertire: ");
+        importo = sc.nextFloat();
+        Valuta v1 = new Valuta("EUR", 1.023f);
+        float x = v1.converti(importo);
+        System.out.println("Il valore in dollari " + x);
 
         // Test del metodo polimorfico: calcolaTassa
+        System.out.println("\nTEST TASSA/POLIMORFISMO");
         ValutaTaxAmeriche vta = new ValutaTaxAmeriche("USD", 1.023f);
-        System.out.println("Tassa di cambio per area Americhe: " + vta.calcolaTassa(1000f));
+        System.out.println("Tassa di cambio per area Americhe: " +
+                vta.calcolaTassa(1000f));
 
         ValutaTaxEuropa vte = new ValutaTaxEuropa("EUR", 1.023f);
-        System.out.println("Tassa di cambio per area Europa: " + vte.calcolaTassa(1000f));
+        System.out.println("Tassa di cambio per area Europa: " +
+                vte.calcolaTassa(1000f));
 
         ValutaTaxAsiaAfrica vtaa = new ValutaTaxAsiaAfrica("USD", 1.023f);
-        System.out.println("Tassa di cambio per area Asia/Africa: " + vtaa.calcolaTassa(1000f));
+        System.out.println("Tassa di cambio per area Asia/Africa: " +
+                vtaa.calcolaTassa(1000f));
 
-      sc.close();
+        // Cambio
+        System.out.println("\nTEST CAMBIO/ARRAY/RIFERIMENTI");
+        Valuta arr[] = new Valuta[4];
+        //Valuta v1 = new Valuta("EUR", 1.023f);
+        Valuta v2 = new Valuta("IND", 2.2f);
+        Valuta v3 = new Valuta("AUD", 0.8f);
+        Valuta v4 = new Valuta("JPY", 1.4f);
+
+        arr[0] = v1;
+        arr[1] = v2;
+        arr[2] = v3;
+        arr[3] = v4;
+
+        // Attenzione!! l'array delle valute, e le valute che esso contiene (i
+        // riferimenti) vengono passati al costruttore per riferimento (indirizzi), 
+        // quindi, ad esempio, una modifica fatta all'oggetto di tipo Valuta, v1 (primo elemento dell'array),
+        // sarà "visibile" (vedi esempio sotto) anche dall'oggetto c1, di tipo Cambio),
+        // essendo, l'array (arr), il medesimo.
+        // La soluzione consiste nel fare le copie degli oggetti, anche dell'array; in
+        // questo contesto, la creazione delle copie può essere fatta nel costruttore
+        // della classe Cambio.
+        Cambio c1 = new Cambio(arr, 4);
+
+        // Esempio modifica "visibile":
+        System.out.println(c1.toString()); // stato prima della modifica
+        v1.setDivisa("XYZ"); // modifica di una valuta
+        System.out.println(c1.toString()); // si osservi che la modifica fatta è "visibile" nell'oggetto c1
+
+        System.out.println("\nTEST CAMBIO/CONVERSIONE");
+        Importo imp = new Importo(v2, 5000); // importo di 5000 IND (v2)
+        // Converto 5000 IND in JPY (v4)
+        System.out.println("Importo di " + imp.getImporto() + " " + imp.getValuta().getDivisa() 
+                + " = " + c1.cambiaValuta(imp, v4).getImporto() + " " + v4.getDivisa());
+
+        sc.close();
     }
 }
--- Valuta.java
+++ Valuta.java
@@ -1,32 +1,57 @@
+/**
+ * Questa classe rappresenta una valuta monetaria vedi:
+ * <a href="../valuta.pdf">valuta.pdf</a>
+ * 
+ * @author 4A ITI a.s. 2024-25
+ * @version %I%, %G%
+ */
 public class Valuta {
-    public String divisa;
-    public float tasso;
-  
-   public Valuta(String divisa, float tasso){
-    this.divisa=divisa;
-    this.tasso=tasso;
-   }
+  public String divisa;
+  public float tasso;
 
-   public void setDivisa(String d){
-     this.divisa=d;
-   }
-   public void setTasso(float t){
-    this.tasso=t;
-   }
-   
-   public String getDivisa(){
+  public Valuta(String divisa, float tasso) {
+    this.divisa = divisa;
+    this.tasso = tasso;
+  }
+
+  /**
+   * Costruttore di copia
+   * @param valuta Oggetto di classe Valuta da copiare
+   */
+  public Valuta(Valuta valuta) {
+    this.divisa = valuta.getDivisa();
+    this.tasso = valuta.tasso;
+  }
+
+  public void setDivisa(String d) {
+    this.divisa = d;
+  }
+
+  public void setTasso(float t) {
+    this.tasso = t;
+  }
+
+  public String getDivisa() {
     return divisa;
-   }
+  }
 
-   //metodo per la conversione del importo in USD utilizzando il tasso di cambio predefinito
-   public float converti (float importo){
-        return importo*this.tasso;
-    }
-    
-    public float calcolaTassa(float importo) {
-      return (float) 0.0;
-    }
+  /**
+   * Metodo per la conversione del importo in USD utilizzando il tasso di cambio
+   * predefinito
+   * 
+   * @param importo
+   * @return Importo convertito nella valuta corrente dell'oggetto.
+   */
+  public float converti(float importo) {
+    return importo * this.tasso;
+  }
+
+  public float calcolaTassa(float importo) {
+    return (float) 0.0;
+  }
+
+  public float getTasso() {
+    return this.tasso;
+  }
 
 }
-    
-
